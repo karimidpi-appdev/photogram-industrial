16014076ebe08afd857516fe3e6fbcdc9d7d11fc
Mohammad Ali Karimi <ali216k@gmail.com>
2023-05-07 16:13:44 +0000

    Set up users#show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root "photos#index"
+
   devise_for :users
+
   resources :comments
   resources :follow_requests
   resources :likes
   resources :photos
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: :show
+  
 end
