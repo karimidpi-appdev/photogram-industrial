3e20d66bc74054360cddc05116c0d24998eb71c4
Mohammad Ali Karimi <ali216k@gmail.com>
2023-05-07 18:36:43 +0000

    Added commenting

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :likes
   resources :photos
-  resources :users, only: :show
+
+  get "/:username" => "users#show"
   
 end
